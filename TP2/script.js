27973ed73cb2eff18f43697b79be2b8e5687efcd
Mhenni12 <medazizmhenni12@gmail.com>
2025-09-28 12:35:05 +0100

    refactor code

--- TP2/script.js
+++ TP2/script.js
@@ -1,37 +1,84 @@
-const taskArray = ["Do homework", "Clean dishes", "Throw dishes"];
+const taskArray = [
+  {
+    texte: "Do homework",
+    etat: "terminee",
+  },
+  {
+    texte: "Wash the dishes",
+    etat: "non-terminee",
+  },
+];
+// Display exisiting tasks initially
 renderTaskGrid();
 
-function AddTask() {
+// Adding eventListener to the add button
+const addButtonElement = document.querySelector(".js-add-button");
+addButtonElement.addEventListener("click", () => {
+  addTask();
+});
+
+function addTask() {
   const inputElement = document.querySelector(".js-task-input");
-  taskArray.push(inputElement.value);
-  console.log(taskArray);
+  const texte = inputElement.value;
+
+  const taskObject = { texte: texte, etat: "non-terminee" };
+
+  taskArray.push(taskObject);
+
+  // Display the updated task list
   renderTaskGrid();
+
+  // Deleting the task from the input element after adding it
+  inputElement.value = "";
 }
 
 function renderTaskGrid() {
-  let html = "";
-  taskArray.forEach((task, index) => {
-    html += `
+  let todoListHTML = "";
+
+  taskArray.forEach((taskObject) => {
+    const { texte, etat } = taskObject;
+
+    todoListHTML += `
     <ul class="task-row">
-      ${task}
+      <div class="text-display ${etat}">
+        ${texte}
+      </div>
       <button 
-        class="task-button delete-button" 
-        onclick="deleteTask(${index})"
+        class="task-button delete-button js-delete-button" 
       >Supprimer</button>
 
       <button 
         class="task-button finish-button js-finish-button" 
-        onclick="finishTask(${index})"
       >Terminer</button>
     </ul>`;
   });
-  const taskGrid = document.querySelector(".js-tasks-list");
-  taskGrid.innerHTML = html;
+  document.querySelector(".js-tasks-list").innerHTML = todoListHTML;
+
+  // Adding eventListeners to the delete buttons
+  document
+    .querySelectorAll(".js-delete-button")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        deleteTask(index);
+      });
+    });
+
+  // Adding eventListeners to the finish buttons
+  document
+    .querySelectorAll(".js-finish-button")
+    .forEach((finishButton, index) => {
+      finishButton.addEventListener("click", () => {
+        finishTask(index);
+      });
+    });
 }
 
-function deleteTask(index) {
-  taskArray.splice(index, 1);
+function deleteTask(taskNumber) {
+  taskArray.splice(taskNumber, 1);
   renderTaskGrid();
 }
 
-function finishTask(index) {}
+function finishTask(taskNumber) {
+  taskArray[taskNumber].etat = "terminee";
+  renderTaskGrid();
+}
