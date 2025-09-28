8a1b2ce4ea42ce6ffb2dabe2e11a2950e46240e8
Mhenni12 <medazizmhenni12@gmail.com>
2025-09-28 12:33:44 +0100

    Add delete and finish button

--- TP2/script.js
+++ TP2/script.js
@@ -10,9 +10,28 @@ function AddTask() {
 
 function renderTaskGrid() {
   let html = "";
-  taskArray.forEach((task) => {
-    html += `<ul>${task}</ul>`;
+  taskArray.forEach((task, index) => {
+    html += `
+    <ul class="task-row">
+      ${task}
+      <button 
+        class="task-button delete-button" 
+        onclick="deleteTask(${index})"
+      >Supprimer</button>
+
+      <button 
+        class="task-button finish-button js-finish-button" 
+        onclick="finishTask(${index})"
+      >Terminer</button>
+    </ul>`;
   });
   const taskGrid = document.querySelector(".js-tasks-list");
   taskGrid.innerHTML = html;
 }
+
+function deleteTask(index) {
+  taskArray.splice(index, 1);
+  renderTaskGrid();
+}
+
+function finishTask(index) {}
