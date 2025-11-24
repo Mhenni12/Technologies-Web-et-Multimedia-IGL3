f7c3c3679e078ca4f1ef421c07c2fad2a282a167
Mhenni12 <medazizmhenni12@gmail.com>
2025-11-24 21:22:40 +0100

    exercice 8

---
+++ TP3 Typescript/exercice8/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+console.log("Add(10, 4):", (0, index_1.add)(10, 4));
+console.log("Subtract(10, 4):", (0, index_1.subtract)(10, 4));
+var user = { id: 1, name: "Aziz" };
+console.log("User:", user);
